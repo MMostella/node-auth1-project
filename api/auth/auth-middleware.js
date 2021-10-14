0d4ea2215d3de20183a3df0b3253e0940eba82ad
MMostella <masonmostella@gmail.com>
2021-10-14 09:39:34 -0500

    fleshed out login endpoint and completed restricted

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,8 +1,11 @@
 const User = require("../users/users-model");
 
 function restricted(req, res, next) {
-  console.log("restricted");
-  next();
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: `You shall not pass!` });
+  }
 }
 
 async function checkUsernameFree(req, res, next) {
@@ -22,6 +25,7 @@ async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
     if (users.length) {
+      req.user = users[0];
       next();
     } else {
       next({ status: 401, message: "Invalid credentials" });
