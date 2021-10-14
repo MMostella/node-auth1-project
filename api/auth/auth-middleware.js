a1a439b62d3b94dcbc3b1d4167a4980e3b67495f
MMostella <masonmostella@gmail.com>
2021-10-14 09:07:43 -0500

    created session and cookie

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,25 +1,10 @@
 const User = require("../users/users-model");
-/*
-  If the user does not have a session saved in the server
 
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
 function restricted(req, res, next) {
   console.log("restricted");
   next();
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
 async function checkUsernameFree(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
@@ -33,14 +18,6 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
@@ -54,19 +31,14 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
 function checkPasswordLength(req, res, next) {
-  next();
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ status: 422, message: `Password must be longer than 3 chars` });
+  } else {
+    next();
+  }
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   checkPasswordLength,
   checkUsernameExists,
